b1f0524d249b2ecd4249e0ac2f17737a00b942bf
Arthur <arthurbm15@gmail.com>
2024-05-29 16:12:17 -0300

    ajustando o arquivo buildspec e colocando o chrome no modo headless

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 def before_all(context):
-    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    # Configurar opções do Chrome
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Ativar modo headless
+    chrome_options.add_argument("--disable-gpu")  # Desativar GPU (recomendado para o modo headless)
+    chrome_options.add_argument("--no-sandbox")  # Desativar o sandbox (necessário para algumas configurações de CI)
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Superar problemas de recursos limitados
+
+    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
 def after_all(context):
     context.driver.close()
